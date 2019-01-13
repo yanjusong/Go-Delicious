8d43970c0cecc4b8e33a1adf502336a25750df0f
yanjusong <1023651409@qq.com>
2019-01-13 12:42:06 +0800

    exec process

---
+++ easy/exec_process/execp.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+
+	binary, lookErr := exec.LookPath("ls")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	fmt.Println(binary)
+
+	args := []string{"ls", "-a", "-l", "-h"}
+
+	env := os.Environ()
+
+	fmt.Println(env)
+
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
+}
