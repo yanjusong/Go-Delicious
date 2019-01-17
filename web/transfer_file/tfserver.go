8e8c65e1e35b0331df44ec1195c4b79a6299b64c
yanjusong <1023651409@qq.com>
2019-01-17 12:10:08 +0800

    文件下载服务器

---
+++ web/transfer_file/tfserver.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println("Starting http file sever")
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/download", handleDownload)
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handleIndex(writer http.ResponseWriter, request *http.Request) {
+	Openfile, err := os.Open("web/index.html")
+	defer Openfile.Close() //Close after function return
+	if err != nil {
+		//File not found, send 404
+		http.Error(writer, "index.html not found.", 404)
+		return
+	}
+
+	io.Copy(writer, Openfile)
+}
+
+func handleDownload(writer http.ResponseWriter, request *http.Request) {
+	//First of check if Get is set in the URL
+	fileurl := request.URL.Query().Get("fileurl")
+	if fileurl == "" {
+		//Get not set, send a 400 bad request
+		http.Error(writer, "Get 'file' not specified in url.", 400)
+		return
+	}
+	fmt.Println("Client requests: " + fileurl)
+
+	//url := "https://github.com/yanjusong/Algorithm/archive/master.zip"
+	resp, err := http.Get(fileurl)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Header)
+
+	writer.Header().Set("Content-Disposition", resp.Header.Get("Content-Disposition"))
+	writer.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	// writer.Header().Set("Content-Length", "416011")
+
+	io.Copy(writer, resp.Body)
+}
