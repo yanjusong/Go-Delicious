fe056855cd077340a97abb12f2158f715b38d9a1
yanjusong <1023651409@qq.com>
2019-01-23 00:16:26 +0800

    file upload demo

---
+++ web/file_upload/fileupload.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
+
+	fmt.Fprintln(w, "\n\nfileHeader content is:")
+	fmt.Fprintln(w, fileHeader)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
