9c772fef9b669137f54f72a1c63333a86e3759ab
yanjusong <1023651409@qq.com>
2019-03-18 14:55:39 +0800

    Mysql 增删改查 事务

---
+++ database/mysql_update/mysql_update.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type Person struct {
+	UserId   int    `db:"user_id"`
+	Username string `db:"username"`
+	Sex      string `db:"sex"`
+	Email    string `db:"email"`
+}
+
+type Place struct {
+	Country string `db:"country"`
+	City    string `db:"city"`
+	TelCode int    `db:"telcode"`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	database, err := sqlx.Open("mysql", "root:3170song@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = database
+}
+
+func main() {
+	res, err := Db.Exec("update person set username=? where user_id=?", "stu0003", 3)
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	// 获取受影响的行数
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("rows failed, ", err)
+	}
+	fmt.Println("update success:", row)
+
+}
