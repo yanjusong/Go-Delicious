17ae702ce3566df45957db1a20fabfb7f53cf184
yanjusong <1023651409@qq.com>
2019-04-08 23:29:47 +0800

    sql-migrate practice

---
+++ mysql-migrate/mysql/003-add-table3.sql
@@ -0,0 +1,7 @@
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE animal (id int);
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE animal;
